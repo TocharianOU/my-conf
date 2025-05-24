788937308916cb6ce2f578fbadcd3d141990a0ba
Luke <134018611+TocharianOU@users.noreply.github.com>
2025-05-25 05:06:44 +0800

    Update init_es.sh

--- init_es.sh
+++ init_es.sh
@@ -9,10 +9,8 @@ done
 
 echo "Elasticsearch is up - applying settings, mappings, and scripts"
 
-# 1. 创建 address_places 索引 (Settings and Mappings)
-echo "Creating/Updating address_places index..."
-curl -X PUT "http://elasticsearch:9200/address_places" -H 'Content-Type: application/json' -d'
-{
+# 1. 定义索引配置
+INDEX_CONFIG='{
   "settings": {
     "index.max_ngram_diff": 99,
     "index.mapping.total_fields.limit": 100000,
@@ -85,48 +83,134 @@ curl -X PUT "http://elasticsearch:9200/address_places" -H 'Content-Type: applica
                 }
               }
             }
+          },
+          "rank": {
+            "type": "integer"
           }
         }
       }
     }
   }
-}
-'
+}'
+
+# 2. 创建索引
+echo "Creating/Updating address_places index..."
+curl -X PUT "http://elasticsearch:9200/address_places" \
+  -H 'Content-Type: application/json' \
+  -d "$INDEX_CONFIG"
 echo ""
 
-# 2. 创建 address_places_search 搜索模板脚本
+# 3. 定义模板 source 变量（用单引号包裹，避免转义）
+ADDRESS_PLACES_SEARCH_SOURCE='{
+  "query": {
+    "nested": {
+      "path": "address_parts",
+      "query": {
+        "function_score": {
+          "query": {
+            "match": {
+              "address_parts.name.name:my": {
+                "query": "{{keyword}}"
+              }
+            }
+          },
+          "functions": [
+            {
+              "script_score": {
+                "script": {
+                  "source": "Math.pow(2, doc[\\\"address_parts.rank\\\"].value / 5)"
+                }
+              }
+            }
+          ],
+          "boost_mode": "multiply"
+        }
+      },
+      "score_mode": "avg",
+      "inner_hits": { "size": 3 }
+    }
+  },
+  "sort": ["_score"],
+  "size": "{{size}}"
+}'
+
+NAME_SEARCH_SOURCE='{
+  "query": {
+    "multi_match": {
+      "fields": ["names.name:my.ngram", "names.name.ngram"],
+      "query": "{{keyword}}"
+    }
+  },
+  "size": "{{size}}"
+}'
+
+UNIVERSAL_NAME_ADDRESS_SEARCH_SOURCE='{
+  "query": {
+    "bool": {
+      "should": [
+        {
+          "multi_match": {
+            "fields": ["names.name:my.ngram", "names.name.ngram"],
+            "query": "{{keyword}}"
+          }
+        },
+        {
+          "nested": {
+            "path": "address_parts",
+            "query": {
+              "function_score": {
+                "query": {
+                  "match": {
+                    "address_parts.name.name:my": {
+                      "query": "{{keyword}}"
+                    }
+                  }
+                },
+                "functions": [
+                  {
+                    "script_score": {
+                      "script": {
+                        "source": "Math.pow(2, doc[\\\"address_parts.rank\\\"].value / 5)"
+                      }
+                    }
+                  }
+                ],
+                "boost_mode": "multiply"
+              }
+            },
+            "score_mode": "avg",
+            "inner_hits": { "size": 3 }
+          }
+        }
+      ]
+    }
+  },
+  "sort": ["_score"],
+  "size": "{{size}}"
+}'
+
+# 4. 组合模板 JSON
+ADDRESS_PLACES_SEARCH_TMPL=$(jq -cn --arg src "$ADDRESS_PLACES_SEARCH_SOURCE" '{script: {lang: "mustache", source: $src}}')
+NAME_SEARCH_TMPL=$(jq -cn --arg src "$NAME_SEARCH_SOURCE" '{script: {lang: "mustache", source: $src}}')
+UNIVERSAL_NAME_ADDRESS_SEARCH_TMPL=$(jq -cn --arg src "$UNIVERSAL_NAME_ADDRESS_SEARCH_SOURCE" '{script: {lang: "mustache", source: $src}}')
+
+# 5. 创建/更新模板
 echo "Creating/Updating _scripts/address_places_search..."
-curl -X POST "http://elasticsearch:9200/_scripts/address_places_search" -H 'Content-Type: application/json' -d'
-{
-  "script": {
-    "lang": "mustache",
-    "source": "{\"query\":{\"nested\":{\"path\":\"address_parts\",\"query\":{\"function_score\":{\"query\":{\"match\":{\"address_parts.name.name:my\":{\"query\":\"{{keyword}}\"}}},\"functions\":[{\"script_score\":{\"script\":{\"source\":\"Math.pow(2, doc[\\\"address_parts.rank\\\"].value / 5)\"}}}],\"boost_mode\":\"multiply\"}},\"score_mode\":\"avg\",\"inner_hits\":{\"size\":3}}},\"sort\":[\"_score\"],\"size\":\"{{size}}\"}"
-  }
-}
-'
+curl -X POST "http://elasticsearch:9200/_scripts/address_places_search" \
+  -H 'Content-Type: application/json' \
+  -d "$ADDRESS_PLACES_SEARCH_TMPL"
 echo ""
 
-# 3. 创建 name_search 搜索模板脚本
 echo "Creating/Updating _scripts/name_search..."
-curl -X POST "http://elasticsearch:9200/_scripts/name_search" -H 'Content-Type: application/json' -d'
-{
-  "script": {
-    "lang": "mustache",
-    "source": "{\"query\":{\"multi_match\":{\"fields\":[\"names.name:my.ngram\",\"names.name.ngram\"],\"query\":\"{{keyword}}\"}},\"size\":\"{{size}}\"}"
-  }
-}
-'
+curl -X POST "http://elasticsearch:9200/_scripts/name_search" \
+  -H 'Content-Type: application/json' \
+  -d "$NAME_SEARCH_TMPL"
 echo ""
 
-# 4. 创建 universal_name_address_search 搜索模板脚本
 echo "Creating/Updating _scripts/universal_name_address_search..."
-curl -X POST "http://elasticsearch:9200/_scripts/universal_name_address_search" -H 'Content-Type: application/json' -d'
-{
-  "script": {
-    "lang": "mustache",
-    "source": "{\"query\":{\"bool\":{\"should\":[{\"multi_match\":{\"fields\":[\"names.name:my.ngram\",\"names.name.ngram\"],\"query\":\"{{keyword}}\"}},{\"nested\":{\"path\":\"address_parts\",\"query\":{\"function_score\":{\"query\":{\"match\":{\"address_parts.name.name:my\":{\"query\":\"{{keyword}}\"}}},\"functions\":[{\"script_score\":{\"script\":{\"source\":\"Math.pow(2, doc[\\\"address_parts.rank\\\"].value / 5)\"}}}],\"boost_mode\":\"multiply\"}},\"score_mode\":\"avg\",\"inner_hits\":{\"size\":3}}}]},\"sort\":[\"_score\"],\"size\":\"{{size}}\"}"
-  }
-}
-'
+curl -X POST "http://elasticsearch:9200/_scripts/universal_name_address_search" \
+  -H 'Content-Type: application/json' \
+  -d "$UNIVERSAL_NAME_ADDRESS_SEARCH_TMPL"
 echo ""
+
 echo "Elasticsearch initialization script finished."
